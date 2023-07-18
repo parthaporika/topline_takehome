fa20addbe2409c4ba01541b94ef1c1b79c7ec774
Partha Porika <parthaporika@Parthas-MacBook-Pro.local>
2023-07-18 00:13:11 -0400

    Ability to search added

--- src/App.js
+++ src/App.js
@@ -3,16 +3,25 @@ import './App.css';
 import Header from './components/Header';
 import ImageLayout from './components/ImageLayout';
 import useImages from './hooks/useImages';
+import { createContext } from 'react';
+
+export const ImageContext = createContext();
 
 function App() {
   const { response, isLoading, error, fetchData } = useImages(`?key=${process.env.REACT_APP_PIXELBAY_API_KEY}`);
-  console.log(response);
+  
+  const value = {
+    response,
+    isLoading,
+    error,
+    fetchData
+  }
 
   return (
-    <>
+    <ImageContext.Provider value={value}>
       <Header></Header>
       <ImageLayout></ImageLayout>
-    </>
+    </ImageContext.Provider>
   );
 }
 
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,31 @@
-import { TextField } from '@mui/material'
-import React from 'react'
+import { Button, TextField } from '@mui/material'
+import React, { useContext, useState } from 'react'
+import { ImageContext } from '../App';
 
 const Search = () => {
+    const [searchValue, setSearchValue] = useState("");
+    const { fetchData } = useContext(ImageContext);
+
+    const handleInputChange = (e) => {
+        setSearchValue(e.target.value);
+    }
+
+    const handleButtonSearch = () => {
+        fetchData(`?key=${process.env.REACT_APP_PIXELBAY_API_KEY}&q=${encodeURIComponent(searchValue)}`)
+        setSearchValue("");
+    }
+
+    const handleEnterSearch = (e) => {
+        if (e.key === 'Enter') {
+            fetchData(`?key=${process.env.REACT_APP_PIXELBAY_API_KEY}&q=${encodeURIComponent(searchValue)}`)
+            setSearchValue("");
+        }
+    }
+
     return(
         <div>
-            <TextField id="outlined-basic" label="Search Images" variant="outlined" />
+            <TextField id="outlined-basic" label="Search Images" variant="outlined" value={searchValue} onChange={handleInputChange} onKeyDown={handleEnterSearch}/>
+            <Button variant='contained' onClick={handleButtonSearch} disabled={!searchValue}>Search</Button>
         </div>
     )
 }
